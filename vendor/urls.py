b6a78221a9574ecb2c389139d61ab70ab076f398
sayooj <sayooj.o12@gmail.com>
2023-12-09 14:04:04 +0530

    vendor password change

--- vendor/urls.py
+++ vendor/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns=[
     path('vendor-profile/<int:user_id>/', VendorProfileView.as_view(), name='vendor-profile'),
     path('vendor-edit/<int:user_id>/', VendorProfileUpdateView.as_view(), name='user-edit'),
-
+    path('change-password/<int:user_id>/', VendorChangePasswordView.as_view(), name='change-password'),
 ]
