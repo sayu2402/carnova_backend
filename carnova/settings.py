4507942cbaea0726e6b7ef3d295771abb9bce048
sayooj <sayooj.o12@gmail.com>
2023-12-21 09:35:54 +0530

    changes saved

--- carnova/settings.py
+++ carnova/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -276,12 +277,12 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 
-AWS_ACCESS_KEY_ID = 'AKIA4RGN7JSMXVEQ7I64'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'carnova'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'ap-south-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_SIGNATURE_NAME = config('AWS_S3_SIGNATURE_NAME')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
+AWS_S3_FILE_OVERWRITE = config('AWS_S3_FILE_OVERWRITE', default=False, cast=bool)
+AWS_DEFAULT_ACL = config('AWS_DEFAULT_ACL', default=None)
+AWS_S3_VERIFY = config('AWS_S3_VERIFY', default=True, cast=bool)
+DEFAULT_FILE_STORAGE = config('DEFAULT_FILE_STORAGE')
