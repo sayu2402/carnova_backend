34629890a70da8d103fbf09ad9283ca522a301a4
sayooj <sayooj.o12@gmail.com>
2024-01-15 12:12:49 +0530

    chat added in the vendor side also

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import MessageList
+from .views import *
 
 urlpatterns = [
-    path('messages/<int:user_id>/', MessageList.as_view(), name='message_list'),
+    path('messages/<int:user_id>/<int:vendor_id>/', MessageList.as_view(), name='user_vendor_messages'),
+    path('booked-vendors/<int:user_id>/', MessageListVendor.as_view(), name='user_booked_vendors_api'),
+    path('message/<int:user_id>/', MessageLists.as_view(), name='message_list'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -7,8 +7,33 @@ from django.db.models import Q
 from .models import Chat
 from .serializers import MessageSerializer
 from accounts.models import *
+from user.models import *
+from accounts.serializer import *
+
+
 
 class MessageList(APIView):
+    def get(self, request, user_id, vendor_id):
+        # Get user and vendor objects
+        user = get_object_or_404(UserAccount, id=user_id)
+        vendor = get_object_or_404(UserAccount, id=vendor_id)
+
+        # Get the list of messages between the user and the vendor
+        messages = Chat.objects.filter(
+            (Q(sender=user, receiver=vendor) | Q(sender=vendor, receiver=user))
+        ).order_by('timestamp')
+
+        # Serialize the list of messages
+        messages_data = MessageSerializer(messages, many=True).data
+
+        return Response({
+            'user_id': user.id,
+            'vendor_id': vendor.id,
+            'messages': messages_data,
+        }, status=status.HTTP_200_OK)
+    
+    
+class MessageListVendor(APIView):
     def get(self, request, user_id):
         user = get_object_or_404(UserAccount, id=user_id)
 
@@ -51,3 +76,56 @@ class MessageList(APIView):
             'authenticated_user_messages': authenticated_user_messages_data
         }, status=status.HTTP_200_OK)
 
+
+
+
+class MessageLists(APIView):
+    def get(self, request, user_id):
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            return Response({'error': 'Invalid user ID format'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = get_object_or_404(UserAccount, id=user_id)
+
+        receivers = set(
+            list(Chat.objects.filter(sender=user).values_list('receiver', flat=True)) +
+            list(Chat.objects.filter(receiver=user).values_list('sender', flat=True))
+        )
+
+        receiver_details = {}
+        for receiver_id in receivers:
+            receiver = get_object_or_404(UserAccount, id=receiver_id)
+
+            messages = Chat.objects.filter(
+                (Q(sender=user, receiver=receiver) | Q(sender=receiver, receiver=user))
+            ).order_by('-timestamp')
+
+            messages_data = MessageSerializer(messages, many=True).data
+
+            receiver_details[receiver.username] = {
+                'receiver_id': receiver.id,
+                'messages': messages_data,
+            }
+
+        total_receivers = len(receivers)
+
+        authenticated_user_messages = Chat.objects.filter(
+            Q(sender=user) | Q(receiver=user)
+        ).order_by('-timestamp')
+
+        authenticated_user_messages_data = MessageSerializer(authenticated_user_messages, many=True).data
+
+
+        user_profile = get_object_or_404(UserProfile, user=user) 
+        user_bookings = Booking.objects.filter(user=user_profile)
+        corresponding_vendors = VendorProfile.objects.filter(booking__in=user_bookings).distinct()
+        vendor_data = VendorModelSerializer(corresponding_vendors, many=True).data
+
+
+        return Response({
+            'total_receivers': total_receivers,
+            'receiver_details': receiver_details,
+            'authenticated_user_messages': authenticated_user_messages_data,
+            'booked_vendors': vendor_data,
+        }, status=status.HTTP_200_OK)
