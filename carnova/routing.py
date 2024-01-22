9733c746598b2e1de0ce8c1f2caa82437e373cf9
sayooj <sayooj.o12@gmail.com>
2024-01-22 21:59:09 +0530

    routing updated for real time notification

--- carnova/routing.py
+++ carnova/routing.py
@@ -4,4 +4,5 @@ from chat.consumers import *
 websocket_urlpatterns = [
     path("ws/chat/<int:sender_id>/<int:receiver_id>/", ChatConsumer.as_asgi()),
     path("ws/online/<int:user_id>/", OnlineStatusConsumer.as_asgi()),
+    path("ws/notification/", NotificationConsumer.as_asgi()),
 ]
